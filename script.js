5ebb47898488e8e842c566f3fbbef66f00a10b15
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2023-10-02 12:09:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ let aboutPage = document.getElementById("about")
 let projectPage = document.getElementById("projects")
 let showContact = document.getElementById("showcontact")
 let contactInfo = document.getElementById("contact")
+let dimScreen = document.getElementById("cover")
 
 document.documentElement.addEventListener("mouseup", (e) => {
   if (!contactInfo.contains(e.target) && contactInfo.style.display == 'block') {
     contactInfo.style.display = 'none'
+    dimScreen.style.display = 'none'
   }
 })
 
 showContact.onclick = () => {
   contactInfo.style.display = 'block'
+  dimScreen.style.display = 'block'
 }
 
 pageLinks.addEventListener("click", (e) => {
